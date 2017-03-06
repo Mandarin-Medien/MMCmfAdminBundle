19573ef001690507321f7146af8ba76a95230407
Christof Hufnagel <ch@mandarin-medien.de>
2017-03-06 16:32:09 +0100

    Updated MandarinMedien\MMCmfAdminBundle\Form\PageType (includes now fields of extended Classes with FormTypeMetaReader)

--- Form/PageType.php
+++ Form/PageType.php
@@ -3,28 +3,24 @@
 namespace MandarinMedien\MMCmfAdminBundle\Form;
 
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\QueryBuilder;
-use MandarinMedien\MMCmfAdminBundle\Form\Types\NodeRouteInlineType;
 use MandarinMedien\MMCmfContentBundle\Form\Type\TemplatableNodeTemplateType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\DependencyInjection\Container;
+use MandarinMedien\MMCmfContentBundle\Form\FormTypeMetaReader;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Forms;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\Router;
-use MandarinMedien\MMCmfAdminBundle\Form\NodeRouteType;
 use MandarinMedien\MMCmfContentBundle\Entity\Page;
 
 class PageType extends AbstractType
 {
 
     protected $container;
+    protected $hiddenFields = array('id');
 
-    public function __construct(Container $container)
+    public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
@@ -39,30 +35,49 @@ class PageType extends AbstractType
         // get class of the current entity for template selection
         $class = get_class($options['data']);
 
+
+        // get class meta data
+        $manager = $this->container->get('doctrine')->getManager();
+        $formTypeReader = new FormTypeMetaReader();
+
+        $metaData = $manager->getClassMetadata($class);
+
+        // loop default fields
+        foreach ($metaData->getFieldNames() as $field) {
+            if (in_array($field, $this->hiddenFields)) continue;
+
+
+            $builder->add($field, $formTypeReader->get($class, $field));
+
+        }
+
+        // loop association fields
+        foreach ($metaData->getAssociationNames() as $field) {
+
+            if (in_array($field, array(
+                'parent',
+                'nodes',
+                'routes',
+                'template'
+            ))) continue;
+
+            $builder->add($field, $formTypeReader->get($class, $field));
+        }
+
+
         /**
          * @var Router $router
          */
         $router = $this->container->get('router');
 
-
         $builder
-            ->add('name')
-            ->add('metaTitle')
             ->add('parent', EntityType::class, array(
                 'class' => Page::class,
                 'required' => false,
                 'query_builder' => function(EntityRepository $repository) {
-                    return $repository->createQueryBuilder('p')
-                       ;
+                    return $repository->createQueryBuilder('p');
                 }
             ))
-            //->add('routes', 'collection')
-            ->add('metaKeywords')
-            ->add('metaDescription')
-            ->add('metaRobots')
-            ->add('metaAuthor')
-            ->add('metaImage')
-            ->add('visible')
             ->add('template', TemplatableNodeTemplateType::class, array('className' => $class ))
 
             ->add('submit', SubmitType::class, array('label' => 'save'))
@@ -75,18 +90,7 @@ class PageType extends AbstractType
                 'attr' => array(
                     'data-target' => $router->generate('mm_cmf_admin_page')
                 )
-            ))
-        ;
-    }
-
-    /**
-     * @param OptionsResolver $resolver
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => Page::class
-        ));
+            ));
     }
 
     /**
